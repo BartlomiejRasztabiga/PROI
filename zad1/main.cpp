6dd23c389e2f02e25151fbc9cc377e790c6cb970
BartlomiejRasztabiga <contact@rasztabiga.me>
2021-03-25 14:54:02 +0100

    Update

--- zad1/main.cpp
+++ zad1/main.cpp
@@ -1,33 +1,20 @@
 #include <iostream>
 #include "Car.h"
 
-void checkFloatPrecision() {
-    const double epsilon = 0.01;
-
-    float i = 0;
-    double j = 0;
-    while (std::abs(i - j) <= epsilon) {
-        i += 1.0f;
-        j += 1.0;
+class ClassWithPtr {
+public:
+    int *ReturnPtr() {
+        int *int_array = new int[9];
+        return int_array;
     }
-    std::cout << "Max float value while keeping precision: " << std::fixed << i << std::endl;
-}
+};
 
-void forceError() {
-    // "Array out of bounds" error
-    int foo[1];
-    for (int i = 0; i <= 1; i++) {
-        foo[i] = i;
-    }
-}
 
 int main() {
-    auto car = Car("Peugeot", "508", 2013);
-    car.info();
+    ClassWithPtr ptr;
+//    int *ptr_arr = ptr.ReturnPtr();
 
-    checkFloatPrecision();
-
-//    forceError();
+    int *int_array = new int[9];
 
     return 0;
 }
